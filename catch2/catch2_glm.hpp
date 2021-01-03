2db266a8914df19426cebd3bb75708f75775334b
Thomas Arcila <thomas.arcila@gmail.com>
2021-01-03 20:28:06 +0100

    catch2: Support for glm types with catch2

---
+++ catch2/catch2_glm.hpp
@@ -0,0 +1,33 @@
+#ifndef DLDT_TOOLS_CATCH2_CATCH2_GLM_HPP_
+#define DLDT_TOOLS_CATCH2_CATCH2_GLM_HPP_
+
+#include <catch2/catch.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+namespace Catch {
+    template<typename genType, int N>
+    struct StringMaker<glm::vec<N, genType>> {
+        static std::string convert(glm::vec<N, genType> const& value ) {
+            return glm::to_string( value );
+        }
+    };
+
+    template<typename genType, int N>
+    struct StringMaker<glm::mat<N, N, genType>> {
+        static std::string convert(glm::mat<N, N, genType> const& value) {
+            return glm::to_string( value );
+        }
+    };
+
+    template<typename genType>
+    struct StringMaker<glm::tquat<genType>> {
+        static std::string convert(glm::tquat<genType> const& value) {
+            return glm::to_string( value );
+        }
+    };
+}
+
+
+#endif // DLDT_TOOLS_CATCH2_CATCH2_GLM_HPP_
