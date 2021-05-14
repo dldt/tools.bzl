28abd0414fc0eb34f1cd9d97202e708dc8e652c3
Thomas Arcila <thomas.arcila@gmail.com>
2021-05-14 20:51:23 +0200

    format: Fix glob with default bazel layout

--- format/common.ps1
+++ format/common.ps1
@@ -1,5 +1,6 @@
 # Split the Get-ChildItem into two calls, so it does not traverse symlinks
 function Get-Ignore-Directories {
     $submodules = & git config --file .gitmodules --get-regexp path | ForEach-Object { ($_ -Split " ")[1] }
-    ".bazel/", $submodules | Where-Object { Test-Path $_ } | Get-Item
+    $bazelfolders = Get-Item -Path "bazel-*"
+    $bazelfolders, $submodules | Where-Object { Test-Path -PathType Container $_ } | Get-Item
 }
